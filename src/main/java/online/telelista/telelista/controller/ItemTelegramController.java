56e50b95b960bca3a6869b4160e20df560a931e8
edulanzarin <edulanzarin@outlook.com>
2025-10-27 16:01:57 -0300

    feature/crud: atualizado o Controller para usar os DTOs e pegar o usuario logado com @AuthenticationPrincipal

--- src/main/java/online/telelista/telelista/controller/ItemTelegramController.java
+++ src/main/java/online/telelista/telelista/controller/ItemTelegramController.java
@@ -1,13 +1,17 @@
 package online.telelista.telelista.controller;
 
+import online.telelista.telelista.dto.CreateItemRequest;
+import online.telelista.telelista.dto.ItemResponse;
 import online.telelista.telelista.model.ItemTelegram;
-// Importamos o nosso novo SERVIÇO
 import online.telelista.telelista.service.ItemTelegramService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/itens")
@@ -17,16 +21,25 @@ public class ItemTelegramController {
     private ItemTelegramService itemTelegramService;
 
     @PostMapping
-    public ResponseEntity<ItemTelegram> cadastrarItem(@RequestBody ItemTelegram itemRecebido) {
-        ItemTelegram itemSalvo = itemTelegramService.cadastrarItem(itemRecebido);
+    public ResponseEntity<ItemResponse> cadastrarItem(
+            @RequestBody CreateItemRequest itemRequest,
+            @AuthenticationPrincipal UserDetails userDetails) {
 
-        return ResponseEntity.status(201).body(itemSalvo);
+        ItemTelegram itemSalvo = itemTelegramService.cadastrarItem(itemRequest, userDetails);
+
+        ItemResponse response = new ItemResponse(itemSalvo);
+
+        return ResponseEntity.status(201).body(response);
     }
 
     @GetMapping
-    public ResponseEntity<List<ItemTelegram>> listarTodos() {
-        List<ItemTelegram> lista = itemTelegramService.listarTodos();
+    public ResponseEntity<List<ItemResponse>> listarTodos() {
+        List<ItemTelegram> listaDeItens = itemTelegramService.listarTodos();
 
-        return ResponseEntity.ok(lista);
+        List<ItemResponse> responseList = listaDeItens.stream()
+                .map(item -> new ItemResponse(item))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(responseList);
     }
 }
