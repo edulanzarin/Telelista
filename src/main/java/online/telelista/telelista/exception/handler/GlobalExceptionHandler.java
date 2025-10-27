1cb83658d50e40ca04d4066d016b3152201347a2
edulanzarin <edulanzarin@outlook.com>
2025-10-27 16:27:33 -0300

    feature/exception: adicionado GlobalExceptionHandler

---
+++ src/main/java/online/telelista/telelista/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package online.telelista.telelista.exception.handler;
+
+import online.telelista.telelista.exception.AccessDeniedException;
+import online.telelista.telelista.exception.ItemNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ItemNotFoundException.class)
+    public ResponseEntity<String> handleItemNotFound(ItemNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDenied(AccessDeniedException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    }
+}
