5a1691d9cf3cf819b8d537924f73a4c298d39cbc
edulanzarin <edulanzarin@outlook.com>
2025-10-28 08:45:17 -0300

    refactor/controller: removido import não utilizada

--- src/main/java/online/telelista/telelista/controller/ItemTelegramController.java
+++ src/main/java/online/telelista/telelista/controller/ItemTelegramController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.stream.Collectors;
 import java.util.UUID;
 import java.util.Optional;
 
