588dffe111f136d07857d91fc43e6a0c9a79b728
edulanzarin <edulanzarin@outlook.com>
2025-10-28 08:40:52 -0300

    refactor/repository: Query otimizada para paginação.

--- src/main/java/online/telelista/telelista/repository/ItemTelegramRepository.java
+++ src/main/java/online/telelista/telelista/repository/ItemTelegramRepository.java
@@ -1,21 +1,27 @@
 package online.telelista.telelista.repository;
 
 import online.telelista.telelista.model.ItemTelegram;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ItemTelegramRepository extends JpaRepository<ItemTelegram, UUID> {
 
-    @Query("SELECT i FROM ItemTelegram i " +
+    @Query(value = "SELECT i FROM ItemTelegram i " +
             "LEFT JOIN Boost b ON b.item = i AND b.dataFim > :dataAtual " +
-            "ORDER BY b.dataFim DESC NULLS LAST")
-    List<ItemTelegram> findAllOrderByActiveBoost(@Param("dataAtual") LocalDateTime dataAtual);
+            "ORDER BY b.dataFim DESC NULLS LAST",
+
+            countQuery = "SELECT COUNT(i) FROM ItemTelegram i")
+
+    Page<ItemTelegram> findAllOrderByActiveBoost(
+            @Param("dataAtual") LocalDateTime dataAtual,
+            Pageable pageable);
 
 }
