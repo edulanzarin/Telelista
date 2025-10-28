5d226937398a4a1a2b89f4d456d318bbfe382d3e
edulanzarin <edulanzarin@outlook.com>
2025-10-28 14:10:46 -0300

    feat/model: adicionado entidade de categorias

---
+++ src/main/java/online/telelista/telelista/model/Categoria.java
@@ -0,0 +1,31 @@
+package online.telelista.telelista.model;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(exclude = "itens")
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<ItemTelegram> itens;
+}
